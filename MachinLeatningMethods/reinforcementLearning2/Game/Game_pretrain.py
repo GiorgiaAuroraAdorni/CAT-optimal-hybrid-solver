1d455b4b637a8669a6ad3694d2c3299653ce6187
Samuel Corecco <kyo@MacBook-Pro-di-Samuel.local>
2023-05-04 11:27:18 +0200

    pretrain 1

--- MachinLeatningMethods/reinforcementLearning2/Game/Game_pretrain.py
+++ MachinLeatningMethods/reinforcementLearning2/Game/Game_pretrain.py
@@ -1,16 +1,10 @@
-import os
-import sys
 import gymnasium as gym
 from gymnasium import spaces
 import numpy as np
 from .Move import executeInstruction
 
 from pylab import *
-from PIL import Image
-
-
-
-
+import random
 
 
 
@@ -60,14 +54,8 @@ def create_big_matrix(inputMat):
 
 
 
-
-
-
-
-
-
-class GameEnvironment(gym.Env):
-    def __init__(self, V,voidMat, max_id, instructions, patterns, num_colors,map_value, n):
+class GameEnvironmentPreTrain(gym.Env):
+    def __init__(self, boards,voidMat, max_id, instructions, patterns, num_colors,map_value, n):
         super().__init__()
 
         # ciò che vede il ML
@@ -79,19 +67,27 @@ class GameEnvironment(gym.Env):
             n,                                          # node_i
             n,                                          # node_j
             len(instructions),                          # instruction_idx
-            4,                                          # length della mossa
+            6,                                          # length della mossa
             len(patterns)                               # pattern_idx
         ])
 
         self.map_value = map_value                      # è la mappa dei valori per valutare un id
-        self.V = V                                      # è la board completa da riprodurre
+        self.boards = boards                            # il totale delle boards su cui allenarci
+        rdm_idx = random.randint(0, len(boards) - 1)
+        self.V = self.boards[rdm_idx]                   # è la board completa da riprodurre
         self.max_id = max_id                            # è l'id della board piena
         self.instructions = instructions                # array di istruzioni
         self.patterns = patterns                        # array di patterns
-        self.n = len(V)                                 # dimensione V in R^{nxn}
+        self.n = len(self.V)                            # dimensione V in R^{nxn}
         self.voidMat = voidMat                          # la board vuota (con anche gli spazi di fuori board)
+        self.old_action = (-1, -1,-1,-1,-1)             # old action
         self.reset()                                    # inizializzare l'id, la current board e gli steps
 
+    def _valid_actions(self, state):
+        
+        valid_actions = [0, 1, 2, 3]
+        return valid_actions
+    
     def get_state(self, state_print=None):
         A = np.copy(self.V)
         if state_print is not None:
@@ -101,13 +97,14 @@ class GameEnvironment(gym.Env):
         C = np.hstack((A, B))
         C = C.flatten()
         return C
-    
 
     # reset, rigenera la board vuota, resetta il current id, resetta gli steps
     def reset(self):
         self.currentMat = np.copy(self.voidMat)         # current board
         self.current_id = 0                             # current id associato alla current board
         self.steps = 0                                  # numero di mosse
+        rdm_idx = random.randint(0, len(self.boards) - 1)
+        self.V = self.boards[rdm_idx]                   # è la board completa da riprodurre
 
         state = self.get_state()
         return state, {}                   
@@ -136,12 +133,12 @@ class GameEnvironment(gym.Env):
         figure(1)
         plt.imshow(C, interpolation='nearest')
         plt.show()
-
+        
     # esegue un azione sulla board
     def execute_instruction(self, action):
         node_i, node_j, instruction, lengthOfInst, pattern = action
         self.current_id, num_new_colored_cells, self.currentMat, legit_move = executeInstruction(self.current_id, node_i, node_j, instruction, lengthOfInst, pattern, self.V, self.currentMat, self.map_value)
-        return num_new_colored_cells, legit_move
+        return num_new_colored_cells
     
 
     # Singolo step, che esegue un azione
@@ -158,29 +155,23 @@ class GameEnvironment(gym.Env):
             state = self.get_state()
             return state, -10, False,False, {'current_id': self.current_id}
         
-        num_new_colored_cells, legit_move = self.execute_instruction((node_i, node_j, instruction, length, pattern))
-
-        if not legit_move:
-            state = self.get_state()
-            return state, -5, False,False, {'current_id': self.current_id}
-        
-        reward = self.calculate_reward(num_new_colored_cells)
+        num_new_colored_cells = self.execute_instruction((node_i, node_j, instruction, length, pattern))
+        reward = self.calculate_reward(num_new_colored_cells,(node_i, node_j, instruction_idx, length, pattern_idx))
         done = self.is_done()
         next_state = np.copy(self.currentMat)
         self.steps += 1
+        info = {'current_id': self.current_id}
 
         state = self.get_state(state_print=next_state)
-        return state, reward, done, False, {'current_id': self.current_id}
+        return state, reward, done, False, info
 
 
     # reward basato su il premiare quanto riesci a colorare valorizzato da quanto presto sei riusito
     # se la colorazione non è avvenuta penalizza (esempio troppi cancellati, colora fuori dalla board)
-    def calculate_reward(self, num_new_colored_cells):
-        multiplier = max(4 - self.steps, 1)
-        if num_new_colored_cells == 0:
-           num_new_colored_cells = -1
-        reward = multiplier * num_new_colored_cells
-        return reward
+    def calculate_reward(self, num_new_colored_cells, action):
+        node_i, node_j, instruction_idx, length, pattern_idx = action
+        
+        return num_new_colored_cells
 
 
     # controllo se è finito il game, cioè se la board è colorata completamente
---
+++ MachinLeatningMethods/reinforcementLearning2/Game/Move_pretrain.py
@@ -0,0 +1,72 @@
+import numpy as np
+import copy
+
+class Instruction:
+    def __init__(self):
+        self.instruction = []
+        self.pattern = []
+        self.len = 0
+
+WRONG_COLOR = -2
+
+def executeInstruction(id, node_i, node_j, instruction_input, lengthOfInst, pattern, mat, actualRes, value_index):
+    
+    tmp_input_mat = copy.deepcopy(actualRes)
+    tmp_id_imput = id
+
+    instruction = instruction_input.copy()
+
+    n = len(mat)
+    idx_pattern = 0
+    idx_istruct = 0
+    i = node_i
+    j = node_j
+    if mat[i][j] == -1:
+        return id,0, tmp_input_mat, False
+    
+
+    min_to_color = 0
+    type_of_inst = instruction[idx_istruct]
+    instruction.pop(0)
+
+    if type_of_inst == 2:
+        lengthOfInst = len(instruction) // 2 + 1
+    
+    
+
+    while True:
+        if actualRes[i][j] == mat[i][j] and pattern[idx_pattern] != mat[i][j]:  
+            actualRes = copy.deepcopy(tmp_input_mat)
+            return tmp_id_imput, 0, actualRes, False
+
+        if actualRes[i][j] != mat[i][j] and pattern[idx_pattern] == mat[i][j]:
+            id = id + 2**value_index[i*n+j]
+            min_to_color += 1
+
+        if mat[i][j] == pattern[idx_pattern]:
+            actualRes[i][j] = pattern[idx_pattern]
+        else:
+            actualRes[i][j] = WRONG_COLOR
+
+        idx_pattern += 1
+        if idx_pattern >= len(pattern):
+            idx_pattern = 0
+
+        i += instruction[idx_istruct]
+        idx_istruct += 1
+        #print(idx_istruct, len(instruction), "\n")
+        j += instruction[idx_istruct]
+        idx_istruct += 1
+
+        if idx_istruct >= len(instruction):
+            idx_istruct = 0
+
+        lengthOfInst -= 1
+        if lengthOfInst == 0:
+            break
+
+        if i >= len(mat) or i < 0 or j >= len(mat) or j < 0 or mat[i][j] == -1:
+            actualRes = copy.deepcopy(tmp_input_mat)
+            return tmp_id_imput, 0, actualRes, False
+
+    return id, min_to_color, actualRes, True
